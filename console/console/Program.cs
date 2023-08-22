357d0e439360f52c357dbb28c17b20459c9ab696
Halmane <durmanovova@gmail.com>
2023-08-22 10:32:17 +0400

    Добавьте файлы проекта.

---
+++ console/console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Бу");
+            /*
+             Тут должен быть какой-то 
+             многострочный
+             комментарий
+             */
+            Console.Write("Какой-то ");
+            Console.WriteLine("текст");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Vladimir";
+            string lastName = "Durmanov";
+            double height = 170;
+            height = 100;
+            float weight = 79;
+            bool isChild = (height < 150 || weight < 40);
+            string info = "Name: " + firstName + "\nLast name: " + lastName + "\nHeight: " + height + "\nWeight: " + weight + "\nIs child: " + isChild;
+            Console.WriteLine(info);
+            Console.ReadLine();
+        }
+    }
+}
