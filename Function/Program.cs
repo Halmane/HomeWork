25b016048a6c1548937675010caf39d1148eb028
Halmane <durmanovova@gmail.com>
2023-08-22 13:13:52 +0400

    .

--- Function/Program.cs
+++ Function/Program.cs
@@ -3,30 +3,25 @@
     return (s.Substring(0, s.Length / 2), s.Substring(s.Length / 2));
 }
 
-static string Shift(string s, Expression expression)
+static string Shift(string s, Func<char, char> expression)
 {
     string newString = "";
     foreach (char c in s) newString += expression(c);
     return newString;
 }
-static string decryptFirstHalf(string firstHalf)
+static string DecryptFirstHalf(string firstHalf)
 {
-    firstHalf = Shift(firstHalf, (char x) => (char)(x + 1));
+    firstHalf = Shift(firstHalf, (x) => (char)(x + 1));
     firstHalf = firstHalf.Replace('5', 's').Replace('4', 'u');
-    firstHalf = Shift(firstHalf, (char x) => (char)(x - 3));
+    firstHalf = Shift(firstHalf, (x) => (char)(x - 3));
     firstHalf = firstHalf.Replace('0', 'o');
     return firstHalf;
 }
 
-static string decryptSecondHalf(string secondHalf)
+static string DecryptSecondHalf(string secondHalf)
 {
-    string reverse = "";
-    for (int i = secondHalf.Length - 1; i > -1; i--)
-    {
-        reverse += secondHalf[i];
-    }
-    secondHalf = reverse;
-    secondHalf = Shift(secondHalf, (char x) => (char)(x - 4));
+    secondHalf = new string(secondHalf.Reverse().ToArray());
+    secondHalf = Shift(secondHalf, (x) => (char)(x - 4));
     secondHalf = secondHalf.Replace('_', ' ');
     return secondHalf;
 }
@@ -34,9 +29,9 @@ static string decryptSecondHalf(string secondHalf)
 
 string text = "F2p)v\"y233{0->c}ttelciFc";
 var splitText = Split(text);
-splitText.firstHalf = decryptFirstHalf(splitText.firstHalf);
-splitText.secondHalf = decryptSecondHalf(splitText.secondHalf);
+splitText.firstHalf = DecryptFirstHalf(splitText.firstHalf);
+splitText.secondHalf = DecryptSecondHalf(splitText.secondHalf);
 Console.WriteLine(splitText.firstHalf + splitText.secondHalf);
 Console.ReadLine();
 
-delegate char Expression(char x);
+
