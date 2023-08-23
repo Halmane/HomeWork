80a3fbbed244e490e91c91fbaa5919dba675a235
Halmane <durmanovova@gmail.com>
2023-08-23 11:07:55 +0400

    create class TV

--- TV/Program.cs
+++ TV/Program.cs
@@ -1,2 +1,24 @@
-﻿
+﻿using TVshow;
 
+var tv = new TV[10];
+
+for(int i = 0; i < 10; i++)
+{
+    tv[i] = new TV();
+    tv[i].GetInfo();
+}
+
+for (int i = 0; i < 10; i++)
+{
+    tv[i].TVOn();
+    tv[i].TVOn();
+    tv[i].TVOn();
+    tv[i].NextChannel();
+    tv[i].LastChannel();
+    tv[i].NextChannel();
+    tv[i].LastChannel();
+    tv[i].NextChannel();
+    tv[i].NextChannel();
+    tv[i].ChangeVolume(15);
+    tv[i].ChangePlaingChannel(15);
+}
--- TV/TV.cs
+++ TV/TV.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TV
+namespace TVshow
 {
     internal class TV
     {
-        string Brand { get; }
-        string Model { get; }
-        double Diagonal { get; }
+        public string Brand { get; }
+        public string Model { get; }
+        public double Diagonal { get; }
         private bool _isOn;
-        Dictionary<int, string> Channels { get; }
-        private int _plaingChannel;
+        private List<string> Channels;
+        private int _playingChannel;
         private int _volume;
         int MaxVolume { get; }
 
@@ -25,10 +25,10 @@ namespace TV
             _isOn = false;
             MaxVolume = 10;
             _volume = 0;
-            FillChannels();
-            _plaingChannel = 1;
+            FillChannels(51);
+            _playingChannel = 1;
         }
-        public TV(string brand, string model, double diagonal) 
+        public TV(string brand, string model, double diagonal, int channels) 
         {
             Brand = brand;
             Model = model;
@@ -36,35 +36,83 @@ namespace TV
             _isOn = false;
             MaxVolume = 10;
             _volume = 0;
-            FillChannels();
-            _plaingChannel = 1;
+            FillChannels(channels);
+            _playingChannel = 1;
         }
 
-        private void FillChannels()
+        private void FillChannels(int count)
         {
-            for(int i = 1; i <= 10; i++) 
+            Channels = new List<string>();
+            for (int i = 1; i <= count; i++) 
             {
-                Channels.Add(i,i.ToString());
+                Channels.Add(GetRandomChannels());
             }
+        }
+
+        private string GetRandomChannels()
+        {
+            Random random = new Random();
+
+            List<string> channel = new List<string> { "МузТВ", "Нтв", "МатчТВ", "Дисней", "Первый", "СТС", "ТНТ", "Nickelodeon" };
+            return channel[random.Next(channel.Count)];
         }
 
         public void TVOn()
         {
             if(_isOn == true) _isOn = false;
             else _isOn = true;
+            Console.WriteLine(_isOn ? "On" : "Off");
         }
 
         public void ChangeVolume(int volume)
         {
+            if(_isOn == false) TVOn();
             _volume += volume;
             if (_volume > MaxVolume) _volume = MaxVolume;
             if(_volume < 0) _volume = 0;
             Console.WriteLine(_volume);
         }
 
-        public void ChangePlaingChanell(int newChanell)
+        public void ChangePlaingChannel(int newChannel)
         {
-            
+            if (_isOn == false) TVOn();
+            if (newChannel <= Channels.Count && newChannel > 0) _playingChannel = newChannel;
+            else _playingChannel = Channels.Count;
+            Console.WriteLine($"Channel:{_playingChannel}-{Channels[_playingChannel]}");
+        }
+
+        public void NextChannel()
+        {
+            if (_isOn == false) TVOn();
+            _playingChannel++;
+            if (_playingChannel > Channels.Count) _playingChannel = 1;
+            Console.WriteLine($"Channel:{_playingChannel}-{Channels[_playingChannel]}");
+        }
+
+        public void LastChannel()
+        {
+            if (_isOn == false) TVOn();
+            _playingChannel--;
+            if (_playingChannel < 1) _playingChannel = Channels.Count;
+            Console.WriteLine($"Channel:{_playingChannel}-{Channels[_playingChannel]}");
+        }
+
+        public void ListOfChannels()
+        {
+            if (_isOn == false) TVOn();
+            for ( int i = 1; i < Channels.Count; i++ )
+            {
+                Console.WriteLine($"Channel:{i}-{Channels[i]}");
+            }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Brand: {Brand} \nModel:{Model} \nDiagonal:{Diagonal}");
+            Console.WriteLine(_isOn ? "On" : "Off");
+            ListOfChannels();
+            Console.WriteLine($"Now playing \nChannel:{_playingChannel}-{Channels[_playingChannel]}");
+            Console.WriteLine($"Volume:{_volume}/{MaxVolume}");
         }
     }
 }
