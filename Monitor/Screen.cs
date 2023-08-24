67691edfa2966415b5a2e4bdd72e0cbc65bc891e
Halmane <durmanovova@gmail.com>
2023-08-24 13:46:42 +0400

    fix

--- Monitor/Screen.cs
+++ Monitor/Screen.cs
@@ -8,20 +8,26 @@ internal class Screen
     public string Matrix { get; }
     public bool Curved { get; }
     public bool IsOn { get; private set; }
-    public List<(Connector, int count)> Connectors;
+    public List<Connector> Connectors;
     public ScreenSettings ScreenSettings;
 
     public Screen(string brand, string model, double diagonal, string matrix, bool curved)
     {
         ScreenSettings = new ScreenSettings(120, 100, 100);
-        Connectors = new List<(Connector, int count)>()
+        Connectors = new List<Connector>()
         {
-            (Connector.HDMI, 1),
-            (Connector.USB2_0, 1),
-            (Connector.USB3_0, 2),
-            (Connector.Jack3_5, 1),
-            (Connector.DisplayPort, 1),
-            (Connector.Type_C, 3)
+            Connector.HDMI,
+            Connector.USB2_0,
+            Connector.USB3_0,
+            Connector.Jack3_5,
+            Connector.DisplayPort,
+            Connector.Type_C,
+            Connector.Type_C,
+            Connector.Type_C,
+            Connector.Type_C,
+            Connector.USB3_0,
+            Connector.USB3_0,
+            Connector.USB3_0
         };
         Brand = brand;
         Model = model;
