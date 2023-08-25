07f14d2f777899b01202240cf89d2ae958ff835d
Halmane <durmanovova@gmail.com>
2023-08-25 15:42:47 +0400

    fix

--- Converter/DefaultConverter.cs
+++ Converter/DefaultConverter.cs
@@ -1,29 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Converter
+﻿namespace Converter;
+public class DefaultConverter : ICurrencyConverter
 {
-    internal class DefaultConverter : ICurrencyConverter
+    public string CurrencyCode { get;}
+    public DefaultConverter(string code)
     {
-        public string CurrencyCode { get;}
-        public DefaultConverter(string code)
-        {
-            CurrencyCode = code;
-        }
+        CurrencyCode = code;
+    }
 
-        public void ConvertRub(double value)
+    public void ConvertRub(double value)
+    {
+        Console.WriteLine("Код не найден, введите курс:");
+        var newConverter = Console.ReadLine();
+        while (!double.TryParse(newConverter, out _) || double.Parse(newConverter) <= 0)
         {
-            Console.WriteLine("Код не найден, введите курс:");
-            var newConverter = Console.ReadLine();
-            while (!double.TryParse(newConverter, out _) || double.Parse(newConverter) <= 0)
-            {
-                Console.WriteLine("Курс не верный,введите заново:");
-                newConverter = Console.ReadLine();
-            }
-            Console.WriteLine($"{value} рублей = {value / double.Parse(newConverter)} {CurrencyCode}");
+            Console.WriteLine("Курс не верный,введите заново:");
+            newConverter = Console.ReadLine();
         }
+        Console.WriteLine($"{value} рублей = {value / double.Parse(newConverter)} {CurrencyCode}");
     }
 }
