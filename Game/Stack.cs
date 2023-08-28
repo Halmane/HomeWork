dc52ef9bdd75b31651c659def337a8e4f8d93bab
Halmane <durmanovova@gmail.com>
2023-08-28 13:21:30 +0400

    stack

---
+++ Game/Stack.cs
@@ -0,0 +1,20 @@
+﻿namespace Game;
+internal class Stack<T> where T : class
+{
+    private List<T> items = new List<T>();
+    public void Push(T item)
+    {
+        items.Add(item);
+    }
+
+    public T? Pop() 
+    {
+        if (IsEmpty()) return null;
+        return items[items.Count - 1];
+    }
+
+    public bool IsEmpty()
+    {
+        return items.Count == 0;
+    }
+}
